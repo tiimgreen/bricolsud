4ed128b1dfd2a600ea0ac2ec445a725a95366937
Tim Green <tiimgreen@gmail.com>
2016-02-09 19:17:49 +0000

    Fix bugs, update for deploy

--- app/models/service_image.rb
+++ app/models/service_image.rb
@@ -3,4 +3,7 @@ class ServiceImage < ActiveRecord::Base
 
   has_attached_file :image, default_url: '/images/:style/missing.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  validates :alt_text, presence: true
+  validates :image,    attachment_presence: true
 end
