0b2fb01f245d438d7e4119189cbe77bc385b2f06
Tim Green <tiimgreen@gmail.com>
2016-02-09 20:45:47 +0000

    Another CSRF fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session, except: :sign_in
 
   before_action :choose_lang, :set_title, :set_page
 
